1b7347c3fbe353c3f27864b1c65dc5394ac57849
Limor Quintal <limor2004@verizon.net>
2018-07-18 10:49:13 -0700

    fixing slider

--- slider.js
+++ slider.js
@@ -5,36 +5,10 @@ $(document).ready(function(){
 		  speed: 500,
 		  autoplay: false,
 		  autoplaySpeed: 5000,
-		  slidesToShow: 4,
+		  slidesToShow: 3,
 		  slidesToScroll: 1,
 		  variableWidth: false,
 		  arrows: true,
-		  responsive: [
-		    {
-		      breakpoint: 1024,
-		      settings: {
-		        slidesToShow: 8,
-		        slidesToScroll: 3,
-		        dots: true
-		      }
-		    },
-		    {
-		      breakpoint: 600,
-		      settings: {
-		        slidesToShow: 8,
-		        slidesToScroll: 1
-		      }
-		    },
-		    {
-		      breakpoint: 480,
-		      settings: {
-		        slidesToShow: 8,
-		        slidesToScroll: 1
-		      }
-		    }
-		    // You can unslick at a given breakpoint now by adding:
-		    // settings: "unslick"
-		    // instead of a settings object
-		  ]
+		  
 		});
   });
