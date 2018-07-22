f364331541f7337a8d6bccffa134c94ccf7a229b
Limor Quintal <limor2004@verizon.net>
2018-07-22 12:12:38 -0700

    change image gallery

--- products/sliderProduct.js
+++ products/sliderProduct.js
@@ -1,19 +1,19 @@
-$(document).ready(function(){
-      $('.imageSlider').slick({
-		  dots: true,
-		  infinite: true,
-		  speed: 500,
-		  autoplay: true,
-		  autoplaySpeed: 5000,
-		  slidesToShow: 3,
-		  slidesToScroll: 3,
-		  variableWidth: false,
-		  arrows: true,
-		  mobileFirst: true,
-		  respondTo: 'imageSlider',
-		  centerMode: true,
-		  focusOnSelect: true,
+// $(document).ready(function(){
+//       $('.imageSlider').slick({
+// 		  dots: true,
+// 		  infinite: true,
+// 		  speed: 500,
+// 		  autoplay: true,
+// 		  autoplaySpeed: 5000,
+// 		  slidesToShow: 3,
+// 		  slidesToScroll: 3,
+// 		  variableWidth: false,
+// 		  arrows: true,
+// 		  mobileFirst: true,
+// 		  respondTo: 'imageSlider',
+// 		  centerMode: true,
+// 		  focusOnSelect: true,
   		  
 		  
-		});
-  });
+// 		});
+//   });
