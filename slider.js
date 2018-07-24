cc1a50a4ddc48e7461fff2055f531549e9122145
Limor Quintal <limor2004@verizon.net>
2018-07-23 18:45:43 -0700

    added product plugin

--- slider.js
+++ slider.js
@@ -2,15 +2,36 @@ $(document).ready(function(){
       $('.slider').slick({
 		  dots: true,
 		  infinite: true,
-		  speed: 500,
+		  speed: 700,
 		  autoplay: true,
-		  autoplaySpeed: 5000,
+		  autoplaySpeed: 10000,
 		  slidesToShow: 3,
 		  slidesToScroll: 3,
 		  variableWidth: false,
 		  arrows: true,
 		  mobileFirst: true,
 		  respondTo: 'slider',
-		  
-		});
+		  responsive: [
+		    {
+		      breakpoint: 1024,
+		      settings: {
+		        slidesToShow: 8,
+		        slidesToScroll: 3,
+		        infinite: true,
+		        dots: true
+		      }
+		    },
+		    {
+		      breakpoint: 768,
+		      settings: {
+		        slidesToShow: 5,
+		        slidesToScroll: 2
+		      }
+		    }
+		    
+		    // You can unslick at a given breakpoint now by adding:
+		    // settings: "unslick"
+		    // instead of a settings object
+		  ]
+				});
   });
