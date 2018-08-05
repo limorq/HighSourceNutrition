2e5c180813b5b720336981268a134a9ce4188165
Limor Quintal <limor2004@verizon.net>
2018-08-05 15:57:04 -0700

    changed file structure

---
+++ slider.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+      $('.slider').slick({
+		  dots: true,
+		  infinite: true,
+		  speed: 700,
+		  autoplay: false,
+		  autoplaySpeed: 10000,
+		  slidesToShow: 3,
+		  slidesToScroll: 3,
+		  variableWidth: false,
+		  arrows: true,
+		  mobileFirst: true,
+		  respondTo: 'slider',
+		  responsive: [
+		    {
+		      breakpoint: 1024,
+		      settings: {
+		        slidesToShow: 8,
+		        slidesToScroll: 3,
+		        infinite: true,
+		        dots: true
+		      }
+		    },
+		    {
+		      breakpoint: 768,
+		      settings: {
+		        slidesToShow: 5,
+		        slidesToScroll: 2
+		      }
+		    }
+		    
+		    // You can unslick at a given breakpoint now by adding:
+		    // settings: "unslick"
+		    // instead of a settings object
+		  ]
+				});
+  });
